34b780c0ab762b9b9215771c2d7f51c7d5857533
Devon VanNote <dvon5150@gmail.com>
2021-12-04 23:18:47 -0600

    Started app style / component setup

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,5 +1,6 @@
 import  React, {Component} from 'react'
 import  ReactDOM from 'react-dom';
 import  Main from './components/Main.js'
+import './styles/styles.css';
 
 ReactDOM.render(<Main />, document.getElementById('root'));

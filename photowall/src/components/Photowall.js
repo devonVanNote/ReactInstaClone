3741575027cf6e110813f924b55eb2d6fa43e707
Devon VanNote <dvon5150@gmail>
2021-12-22 22:58:26 -0600

    Finished up re-rendering the UI by updating the state using the Remove button

--- photowall/src/components/Photowall.js
+++ photowall/src/components/Photowall.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Photo from './Photo'
+import PropTypes from 'prop-types'
 
 function Photowall(props) {
     return (
         <div className="photoGrid">
-            {props.posts.map((post, index) => <Photo post={post} key={index}/>)}
+            {props.posts.map((post, index) => <Photo post={post} onRemovePhoto={props.onRemovePhoto}  key={index}/>)}
         </div>
     );
+}
+
+Photowall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default Photowall;
